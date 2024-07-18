a9bcace6847557cc85f4a47d2f70df94c91cf099
Shreyanshkapoor <161735313+Shreyanshkapoor@users.noreply.github.com>
2024-07-18 16:54:30 +0530

    Add files via upload

---
+++ Adding_Array.java
@@ -0,0 +1,16 @@
+public class Adding_Array {
+    public static void main(String[] args){
+        int[][] A={{1,2,3},{4,5,6},{7,8,9}};
+        int[][] B={{1,3,8},{7,2,6},{3,8,9}};
+        int[][] C=new int[3][3];
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                C[i][j]=A[i][j]+B[i][j];
+                System.out.print(C[i][j]+" ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
