b8567b91c69c98b7330b94d4d982e49e8c56a660
Shreyanshkapoor <161735313+Shreyanshkapoor@users.noreply.github.com>
2024-07-19 09:02:38 +0530

    Update copying_array.java
    change_01

--- copying_array.java
+++ copying_array.java
@@ -1,55 +1,55 @@
-//import javax.swing.text.html.StyleSheet;
-//
-//public class copying_array {
-//    public static void main(String[] args){
-//        int[] A={8,6,10,9,2,15,7,13,14,11};
-//        int[] B=new int[10];
-//        for(int i=0;i<A.length;i++){
-//            B[i]=A[i];
-//            System.out.print(B[i]+",");
+import javax.swing.text.html.StyleSheet;
+
+public class copying_array {
+   public static void main(String[] args){
+       int[] A={8,6,10,9,2,15,7,13,14,11};
+       int[] B=new int[10];
+       for(int i=0;i<A.length;i++){
+           B[i]=A[i];
+           System.out.print(B[i]+",");
+       }
+       System.out.println(" ");
+//        for(int i=0;i<10;i++){
+//            System.out.print(A[i]+",");
 //        }
-//        System.out.println(" ");
-////        for(int i=0;i<10;i++){
-////            System.out.print(A[i]+",");
-////        }
-//    }
-//
-//}
+   }
+
+}
 
 
-//
-//// Reverse copying in array
-//public class copying_array{
-//    public static void main(String[] args){
-//        int[] A={8,6,10,9,2,15,7,13,14,11};
-//        int[] B=new int[10];
-//        for(int i=A.length-1,j=0;i>=0;i--,j++){
-//            B[j]=A[i];
-//        }
-//        for(int x:B){
-//            System.out.print(x+",");
-//        }
-//
-//    }
-//}
 
-// increase the array.
+// Reverse copying in array
 public class copying_array{
-    public static void main(String[] args){
-         int[] A={8,6,10,9,2};
-         int[] B=new int[2*A.length];
-         System.out.println("length before modification of A:"+A.length);
-         for (int i=0;i<A.length;i++){
-             B[i]=A[i];
-             System.out.print(B[i]+",");
-         }
-         System.out.println(" ");
-         A=B;
-         B=null;
-         System.out.println("Length after modifiction of A :"+A.length);
+   public static void main(String[] args){
+       int[] A={8,6,10,9,2,15,7,13,14,11};
+       int[] B=new int[10];
+       for(int i=A.length-1,j=0;i>=0;i--,j++){
+           B[j]=A[i];
+       }
+       for(int x:B){
+           System.out.print(x+",");
+       }
+
+   }
+}
+
+// // increase the array.
+// public class copying_array{
+//     public static void main(String[] args){
+//          int[] A={8,6,10,9,2};
+//          int[] B=new int[2*A.length];
+//          System.out.println("length before modification of A:"+A.length);
+//          for (int i=0;i<A.length;i++){
+//              B[i]=A[i];
+//              System.out.print(B[i]+",");
+//          }
+//          System.out.println(" ");
+//          A=B;
+//          B=null;
+//          System.out.println("Length after modifiction of A :"+A.length);
 
 
 
-    }
+//     }
 }
 
